f52bbd398be9ad3cd36dda11a4fa924f54687953
猴貓 <a102009102009@gmail.com>
2024-06-14 22:00:56 +0800

    feat highlight

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,9 @@ process
 
 const chat = new LiveChat(IDUrl, interval);
 
+console.log("\x1b[35m%s\x1b[0m", "正在監聽直播間...");
+console.log("\x1b[35m%s\x1b[0m", `直播間 ID: ${IDUrl}`);
+
 chat
   .on("chat", (data) => {
     console.log(
@@ -25,7 +28,7 @@ chat
   .on("newMembership", ({ MembershipType }) => {
     if (!MembershipType) return;
 
-    console.log("新的會員:", MembershipType);
+    console.log("\x1b[36m%s\x1b[0m", `新的會員:${MembershipType}`);
 
     addTime(`+:${MembershipLevel?.[MembershipType] || 0}`);
   })
@@ -34,9 +37,11 @@ chat
     if (!baseAmountValue) return;
 
     console.log(
+      "\x1b[36m",
       "新的超級留言:",
       `${currency ? currency + " " : ""}${amountValue} ->`,
-      baseAmountValue
+      baseAmountValue,
+      "\x1b[0m"
     );
 
     addTime(`+:${baseAmountValue * Proportion}`);
